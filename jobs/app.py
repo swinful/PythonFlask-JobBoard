0e3d890ad939b72da308dc9344fe2fbb5a4eb978
Samuel A Winful <samuel@winful.com>
2022-02-21 00:25:04 -0500

    Preparing for Dynamic Content

--- jobs/app.py
+++ jobs/app.py
@@ -1,8 +1,47 @@
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, render_template, g
+
+PATH = 'db/jobs.sqlite'
 
 # Instance of the Flask class called app w/ special variable __name__ passed to
 # the Flask class constructor.
 app = Flask(__name__)
+
+def open_connection():
+    """Opens connection to database"""
+    connection = getattr(g, '_connection', None)
+
+    if connection is None:
+        connection = g._connection = sqlite3.connect(PATH)
+
+    connection.row_factory = sqlite3.Row
+    return connection
+
+
+def execute_sql(sql, values=(), commit=False, single=False):
+    """An easier way to query the database"""
+    connection = open_connection()
+
+    cursor = connection.execute(sql, values)
+
+    if commit is True:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+
+    cursor.close()
+    return results
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    """Ensure database connection is closed when app_context is torn down"""
+    connection = getattr(g, '_connection', None)
+
+    if connection is not None:
+        connection.close()
+
+    return connection
 
 
 @app.route('/')
